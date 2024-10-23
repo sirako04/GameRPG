30eae811bf255f54da52ceb4ed442ae2e0f8062f
Sirako <karissirak6@gmail.com>
2024-10-23 14:01:24 +0200

    almost functioning game

--- EngineRPG/EngineRPG/Player.cs
+++ EngineRPG/EngineRPG/Player.cs
@@ -9,11 +9,12 @@ namespace EngineRPG
 {
 public class Player : LivingCreature
 {
- public int Gold {get; set;}
- public int ExperiencePoints {get; set;}
- public int Level {get; set;}
-        public List<InventoryItem> Inventory {get; set;}
-        public List<PlayerQuest> Quests {get; set;}
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public Location CurrentLocation { get; set; }
+        public List<InventoryItem> Inventory { get; set; } 
+        public List<PlayerQuest> Quests { get; set; }
 
         public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level) :base(currentHitPoints, maximumHitPoints)
         {
